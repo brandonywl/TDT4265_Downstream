f21dac762dff9f2b0ebae716f3be4e5c2357ef3e
brandonywl <brandonywl@gmail.com>
2022-03-25 11:36:16 +0100

    Fixed minor issue again

--- assignment4/SSD/ssd/modeling/backbones/basic_improved.py
+++ assignment4/SSD/ssd/modeling/backbones/basic_improved.py
@@ -4,14 +4,14 @@ from typing import Tuple, List
 
 
 class ResBlock(torch.nn.Module):
-    def __init__(self, in_channels, out_channels, filter_size=(3,3), padding=1, stride=1):
+    def __init__(self, in_channels, out_channels, kernel_size=(3,3), padding=1, stride=1):
         super().__init__()
         self.model = nn.Sequential(
-            nn.Conv2d(in_channels=in_channels, out_channels=in_channels*2, kernel_size=filter_size, padding=padding, stride=stride),
+            nn.Conv2d(in_channels=in_channels, out_channels=in_channels*2, kernel_size=kernel_size, padding=padding, stride=stride),
             nn.ReLU(),
-            nn.Conv2d(in_channels=in_channels*2, out_channels=in_channels*4, kernel_size=filter_size, padding=padding, stride=stride),
+            nn.Conv2d(in_channels=in_channels*2, out_channels=in_channels*4, kernel_size=kernel_size, padding=padding, stride=stride),
             nn.ReLU(),
-            nn.Conv2d(in_channels=in_channels*4, out_channels=in_channels*8, kernel_size=filter_size, padding=padding, stride=stride),
+            nn.Conv2d(in_channels=in_channels*4, out_channels=in_channels*8, kernel_size=kernel_size, padding=padding, stride=stride),
             nn.ReLU(),
             nn.MaxPool2d((2,2)),
             nn.BatchNorm2d(in_channels*8),
